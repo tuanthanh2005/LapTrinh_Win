8e378981b07462ee427e221f26327759f9a05fa7
tranthanhtuan12cb6 <tranthanhtuan12cb6@gmail.com>
2024-03-24 23:38:41 +0700

    code 1

---
+++ bt09_win/Học Viên.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT8
+{
+    class Học_Viên
+    {
+        public int MaHV { get; set; }
+        public string TenHocVien { get; set; }
+        public string TenLopHoc { get; set; }
+        public string MaLH { get; set; }
+    }
+}
